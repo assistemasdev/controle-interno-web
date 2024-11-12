d9e9747d68c540ab9ad35026f6c59ebf51bc5864
gustavasconcelos <gustavo.vasconcelos.peixoto18@gmail.com>
2024-11-12 10:03:41 -0300

    feat: adiciona componente para rotas privadas

--- src/routes.js
+++ src/routes.js
@@ -1,25 +1,36 @@
+// src/routes.js
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import { AuthProvider } from './context/AuthContext'; 
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Login from './pages/Login';
 import Dashboard from './pages/Dashboard';
 import UsersPage from './pages/users/UsersPage';
-import CreateUserPage
- from './pages/users/CreateUserPage';
-const App = () => {
+import CreateUserPage from './pages/users/CreateUserPage';
+import { AuthProvider } from './context/AuthContext';
+import PrivateRoute from './components/PrivateRoute'; 
+
+const AppRoutes = () => {
   return (
-    <AuthProvider>  
+    <AuthProvider>
       <Router>
         <Routes>
           <Route path="/login" element={<Login />} />
-          <Route path="/dashboard" element={<Dashboard />} />
-          <Route path="/usuarios" element={<UsersPage />} />
-          <Route path="/usuarios/criar" element={<CreateUserPage />} /> 
+
+          <Route
+            path="/dashboard"
+            element={<PrivateRoute element={Dashboard} />}
+          />
+          <Route
+            path="/usuarios"
+            element={<PrivateRoute element={UsersPage} />}
+          />
+          <Route
+            path="/usuarios/criar"
+            element={<PrivateRoute element={CreateUserPage} />}
+          />
         </Routes>
       </Router>
     </AuthProvider>
   );
 };
 
-export default App;
-
+export default AppRoutes;
