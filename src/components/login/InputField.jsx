9d8662aeb4b484a9fea22a0b0be33e7bd3c79479
gustavasconcelos <gustavo.vasconcelos.peixoto18@gmail.com>
2024-12-06 16:44:15 -0300

    feat: volta tabwidth

--- src/components/login/InputField.jsx
+++ src/components/login/InputField.jsx
@@ -3,37 +3,38 @@ import { faEye, faEyeSlash } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 const InputField = ({ type, id, placeholder, value, onChange, error }) => {
-    const [isPasswordVisible, setIsPasswordVisible] = useState(false);
+  const [isPasswordVisible, setIsPasswordVisible] = useState(false);
 
-    const togglePasswordVisibility = () => {
-        setIsPasswordVisible(!isPasswordVisible);
-    };
+  const togglePasswordVisibility = () => {
+    setIsPasswordVisible(!isPasswordVisible);
+  };
 
-    return (
-        <div className="form-group" style={{ position: 'relative' }}>
-            <input
-                type={isPasswordVisible ? 'text' : type}  
-                className={`form-control form-control-user ${error ? 'is-invalid' : ''}`}
-                id={id}
-                placeholder={placeholder}
-                value={value}
-                onChange={onChange}
-            />
-            {type === 'password' && (  
-                <FontAwesomeIcon
-                    icon={isPasswordVisible ? faEyeSlash : faEye}
-                    onClick={togglePasswordVisibility}
-                    style={{
-                        position: 'absolute',
-                        right: '15px',
-                        top: '50%',
-                        transform: 'translateY(-50%)',
-                        cursor: 'pointer',
-                    }}
-                />
-            )}
-        </div>
-    );
+  return (
+    <div className="form-group" style={{ position: 'relative' }}>
+      <input
+        type={isPasswordVisible ? 'text' : type}  
+        className={`form-control form-control-user ${error ? 'is-invalid' : ''}`}
+        id={id}
+        placeholder={placeholder}
+        value={value}
+        onChange={onChange}
+      />
+      {type === 'password' && (  
+        <FontAwesomeIcon
+          icon={isPasswordVisible ? faEyeSlash : faEye}
+          onClick={togglePasswordVisibility}
+          style={{
+            position: 'absolute',
+            right: '15px',
+            top: '50%',
+            transform: 'translateY(-50%)',
+            cursor: 'pointer',
+          }}
+        />
+      )}
+      {error && <div className="invalid-feedback">{error}</div>}
+    </div>
+  );
 };
 
 export default InputField;
--- src/context/ApplicationContext.jsx
+++ src/context/ApplicationContext.jsx
@@ -4,42 +4,43 @@ import { useNavigate } from 'react-router-dom';
 export const ApplicationContext = createContext();
 
 export const ApplacationProvider = ({ children }) => {
-    const [selectedApplication, setApplication] = useState(null); 
-    const [loading, setLoading] = useState(true); 
-    const navigate = useNavigate();
+  const [selectedApplication, setApplication] = useState(null); 
+  const [loading, setLoading] = useState(true); 
+  const navigate = useNavigate();
   
-    const selectApplication = (application) => {
+  const selectApplication = (application) => {
+    setApplication(application);
+    localStorage.setItem('selectedApplication', JSON.stringify(application));
+    setLoading(false); 
+  };
+
+  const removeApplication = () => {
+    setApplication(null);
+    localStorage.removeItem("selectedApplication");
+    navigate("/aplicacoes"); 
+  };
+
+  useEffect(() => {
+    const savedApplication = localStorage.getItem('selectedApplication');
+    
+    if (savedApplication) {
+      try {
+        const application = JSON.parse(savedApplication);
         setApplication(application);
-        localStorage.setItem('selectedApplication', JSON.stringify(application));
+        setLoading(false);
+      } catch (error) {
+        console.error("Erro ao carregar a aplicação do localStorage:", error);
         setLoading(false); 
-    };
+      }
+    } else {
+      navigate('/aplicacoes'); 
+    }
+  }, []);
 
-    const removeApplication = () => {
-        setApplication(null);
-        localStorage.removeItem("selectedApplication");
-        navigate("/aplicacoes"); 
-    };
-
-    useEffect(() => {
-        const savedApplication = localStorage.getItem('selectedApplication');
-        
-        if (savedApplication) {
-            try {
-                const application = JSON.parse(savedApplication);
-                setApplication(application);
-                setLoading(false);
-            } catch (error) {
-                console.error("Erro ao carregar a aplicação do localStorage:", error);
-                setLoading(false); 
-            }
-        } else {
-            navigate('/aplicacoes'); 
-        }
-    }, []);
-
-    return (
-        <ApplicationContext.Provider value={{ selectedApplication, selectApplication, removeApplication, loading }}>
-            {children}
-        </ApplicationContext.Provider>
-    );
+  
+  return (
+    <ApplicationContext.Provider value={{ selectedApplication, selectApplication, removeApplication, loading }}>
+      {children}
+    </ApplicationContext.Provider>
+  );
 };
