26b11a90cccef1d0a5c8e5b16f9ec81a59dbd479
gustavasconcelos <gustavo.vasconcelos.peixoto18@gmail.com>
2025-02-28 08:18:47 -0300

    fix: corrige loop de requisições

--- src/context/AuthContext.jsx
+++ src/context/AuthContext.jsx
@@ -8,93 +8,89 @@ import { entities } from '../constants/entities';
 export const AuthContext = createContext();
 
 export const AuthProvider = ({ children }) => {
-  const [isAuthenticated, setIsAuthenticated] = useState(false);
-  const [user, setUser] = useState(null);
-  const [loading, setLoading] = useState(true);
-  const navigate = useNavigate();
-  const { post: userLogout } = useBaseService(navigate);
+    const [isAuthenticated, setIsAuthenticated] = useState(false);
+    const [user, setUser] = useState(null);
+    const [loading, setLoading] = useState(true);
+    const navigate = useNavigate();
+    const { post: userLogout } = useBaseService(navigate);
 
-  useEffect(() => {
-    const token = localStorage.getItem('token');
-    const storedUser = JSON.parse(localStorage.getItem('user'));
+    useEffect(() => {
+        const token = localStorage.getItem('token');
+        const storedUser = JSON.parse(localStorage.getItem('user'));
 
-    const checkAuth = () => {
-      if (token && storedUser) {
-        try {
-          const decodedToken = jwtDecode(token);
+        if (token && storedUser) {
+            try {
+                const decodedToken = jwtDecode(token);
 
-          // Verifica se o token está expirado
-          if (decodedToken.exp * 1000 < Date.now()) {
-            logout();
-            navigate('/login');
-          } else {
-            setIsAuthenticated(true);
-            setUser(storedUser);
-          }
-        } catch (error) {
-          console.log('Erro ao decodificar o token:', error);
-          // Se houver erro no decoding, logout, mas apenas se o usuário já estiver logado
-          if (user) {
-            logout();
-            navigate('/login', { state: { message: 'Sua sessão expirou. Por favor, faça login novamente.' } });
-          }
+                if (decodedToken.exp * 1000 < Date.now()) {
+                    localStorage.clear();
+                    setIsAuthenticated(false);
+                    setUser(null);
+                    setLoading(false);
+                    navigate('/login');
+                } else {
+                    setIsAuthenticated(true);
+                    setUser(storedUser);
+                    setLoading(false);
+                }
+            } catch (error) {
+                console.log('Erro ao decodificar o token:', error);
+                localStorage.clear();
+                setIsAuthenticated(false);
+                setUser(null);
+                setLoading(false);
+                navigate('/login', { state: { message: 'Sua sessão expirou. Por favor, faça login novamente.' } });
+            }
+        } else {
+            setLoading(false);
         }
-      } else {
-        // Se não houver token ou usuário armazenado, não faz nada
-        setLoading(false);
-      }
+    }, [navigate]);
+
+    const login = (token) => {
+        const decoded = jwtDecode(token);
+        const userData = {
+            id: decoded.user_id,
+        };
+
+        localStorage.setItem('token', token);
+        localStorage.setItem('user', JSON.stringify(userData));
+        setIsAuthenticated(true);
+        setUser(userData);
     };
 
-    checkAuth();
-
-    // Define loading como false após a verificação
-    setLoading(false);
-  }, [user, navigate]);
-
-  const login = (token) => {
-    const decoded = jwtDecode(token);
-    const userData = {
-      id: decoded.user_id,
+    const logout = async () => {
+        try {
+            await userLogout(entities.logout.create);
+        } catch (error) {
+            console.error('Erro ao desconectar do servidor:', error);
+        } finally {
+            localStorage.clear();
+            setIsAuthenticated(false);
+            setUser(null);
+            document.documentElement.style.setProperty('--primary-color', '#4da8ff');
+            navigate('/login');
+        }
     };
 
-    localStorage.setItem('token', token);
-    localStorage.setItem('user', JSON.stringify(userData));
-    setIsAuthenticated(true);
-    setUser(userData);
-  };
-
-  const logout = async () => {
-    try {
-      await userLogout(entities.logout.create);
-    } catch (error) {
-      console.error('Erro ao desconectar do servidor:', error);
-    } finally {
-      localStorage.clear();
-      setIsAuthenticated(false);
-      setUser(null);
-      document.documentElement.style.setProperty('--primary-color', '#4da8ff');
+    if (loading) {
+        return (
+            <div
+                style={{
+                    width: '100vw',
+                    height: '100vh',
+                    display: 'flex',
+                    justifyContent: 'center',
+                    alignItems: 'center',
+                }}
+            >
+                <CircularProgress />
+            </div>
+        );
     }
-  };
 
-  if (loading) {
     return (
-      <div
-        style={{
-          width: '100vw',
-          height: '100vh',
-          display: 'flex',
-          justifyContent: 'center',
-          alignItems: 'center',
-        }}
-      >
-        <CircularProgress />
-      </div>
+        <AuthContext.Provider value={{ isAuthenticated, user, login, logout, loading }}>
+            {children}
+        </AuthContext.Provider>
     );
-  }
-
-  return (
-    <AuthContext.Provider value={{ isAuthenticated, user, login, logout, loading }}>
-      {children}
-    </AuthContext.Provider>
-  );
 };
